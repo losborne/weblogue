8a9100a20b2a8b4ef9858f7f967baa3bc3f6eeac
Luke Osborne <lukewosborne@gmail.com>
2018-11-23 11:33:32 -0500

    UI rework

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,34 @@ fn index(connection: DbConn) -> Template {
 
     let post_list = posts.load::<Post>(&*connection).expect("Error loading posts");
 
-    context.insert("posts", &post_list);
+    println!("Loaded {} posts.", post_list.len());
+
+    context.insert("post", &post_list.first());
     Template::render("index", &context)
+}
+
+#[get("/posts")]
+fn posts(connection: DbConn) -> Template {
+    use schema::posts::dsl::*;
+
+    let mut context = Context::new();
+
+    let post_list = posts.load::<Post>(&*connection).expect("Error loading posts");
+
+    context.insert("posts", &post_list);
+    Template::render("posts", &context)
+}
+
+#[get("/about")]
+fn about() -> Template {
+    let mut context = Context::new();
+    Template::render("about", &context)
 }
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .manage(create_db_pool_and_seed())
-        .mount("/", routes![index])
+        .mount("/", routes![index, posts, about])
         .mount("/", StaticFiles::from("static"))
         .attach(Template::fairing())
 }
