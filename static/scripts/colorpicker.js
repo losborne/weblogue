39d072bb42724a2a8e8cd5170da87a9e86c78a9d
Luke Osborne <lukewosborne@gmail.com>
2018-11-24 11:37:33 -0500

    style fixups

--- static/scripts/colorpicker.js
+++ static/scripts/colorpicker.js
@@ -1,6 +1,6 @@
 var colors = ["amber", "light-amber", "green-1", "apple-green", "green-2", "apple-2c", "green-3"];
 
-var color = getCookie("color")
+var color = getCookie("color");
 if (color) {
   changeColor(color);
 } else {
@@ -10,11 +10,11 @@ if (color) {
 function getCookie(cookieName) {
   var name = cookieName + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
-  var cookiePairs = decodedCookie.split(';');
+  var cookiePairs = decodedCookie.split(";");
   for(var i = 0; i < cookiePairs.length; i++) {
     var cookiePair = cookiePairs[i];
     cookiePair = removeLeadingWhitespace(cookiePair);
-    if (cookiePair.indexOf(name) == 0) {
+    if (cookiePair.indexOf(name) === 0) {
       return cookiePair.substring(name.length, cookiePair.length);
     }
   }
@@ -24,17 +24,17 @@ function getCookie(cookieName) {
 function updateColor(color) {
   return function() {
     document.cookie = "color=" + color + ";path=/";
-    changeColor(color)
+    changeColor(color);
   }
 }
 
 function changeColor(color) {
   var root = document.documentElement;
-  root.style.setProperty('--text-color', 'var(--' + color + ')');
+  root.style.setProperty("--text-color", "var(--" + color + ")");
 }
 
 function removeLeadingWhitespace(string) {
-  while (string.charAt(0) == ' ') {
+  while (string.charAt(0) === " ") {
     string = string.substring(1);
   }
   return string;
@@ -44,5 +44,5 @@ colors.forEach(addListener);
 
 function addListener(color, index) {
   var colorClass = document.getElementsByClassName(color)[0];
-  colorClass.addEventListener('click', updateColor(color), false);
+  colorClass.addEventListener("click", updateColor(color), false);
 }
